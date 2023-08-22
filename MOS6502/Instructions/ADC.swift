2661edd16aa49b69ff4c0439f3080a5f95cdc249
macmade <macmade@xs-labs.com>
2023-08-22 16:31:25 +0200

    feat: ADC

--- MOS6502/Instructions/ADC.swift
+++ MOS6502/Instructions/ADC.swift
@@ -47,5 +47,25 @@ import Foundation
  */
 public func ADC( cpu: CPU, context: AddressingContext ) throws
 {
-    throw RuntimeError( message: "Instruction not implemented" )
+    let base        = cpu.registers.A
+    let add         = try context.read()
+    let carry       = cpu.registers.PS.contains( .carryFlag ) ? UInt8( 1 ) : UInt8( 0 )
+    let result      = UInt16( cpu.registers.A ) + UInt16( add ) + UInt16( carry )
+    cpu.registers.A = UInt8( result & 0xFF )
+
+    if base & ( 1 << 7 ) == 0, add & ( 1 << 7 ) == 0, cpu.registers.A & ( 1 << 7 ) != 0
+    {
+        cpu.setFlag( .overflowFlag )
+    }
+    else if base & ( 1 << 7 ) != 0, add & ( 1 << 7 ) != 0, cpu.registers.A & ( 1 << 7 ) == 0
+    {
+        cpu.setFlag( .overflowFlag )
+    }
+    else
+    {
+        cpu.clearFlag( .overflowFlag )
+    }
+
+    cpu.setFlag( result > 0xFF, for: .carryFlag )
+    cpu.setZeroAndNegativeFlags( for: cpu.registers.A )
 }
