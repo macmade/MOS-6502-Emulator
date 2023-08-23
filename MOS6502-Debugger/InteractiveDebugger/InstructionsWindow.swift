924585683815003287b5700dcaab88f3ff02339b
macmade <macmade@xs-labs.com>
2023-08-23 10:12:41 +0200

    feat(Debugger): Instructions and disassembly are now in a single window.

--- MOS6502-Debugger/InteractiveDebugger/InstructionsWindow.swift
+++ MOS6502-Debugger/InteractiveDebugger/InstructionsWindow.swift
@@ -23,14 +23,155 @@
  ******************************************************************************/
 
 import Foundation
+import MOS6502
 import SwiftCurses
+import xasm65lib
 
-public class InstructionsWindow: DisassemblyWindow
+public class InstructionsWindow: DebuggerWindow
 {
+    public override init( computer: Computer, frame: Rect, style: ManagedWindow.Style, prompt: PromptWindow )
+    {
+        super.init( computer: computer, frame: frame, style: style, prompt: prompt )
+    }
+
     public override func render( on window: ManagedWindow )
     {
         window.printLine( foreground: .blue, text: "Instructions:" )
         window.separator()
-        self.printDisassembly( window: window, lines: Int( window.bounds.size.height ) - 2, options1: [ .address ], options2: [ .bytes ] )
+        self.printDisassembly( window: window, lines: Int( window.bounds.size.height ) - 2 )
+    }
+
+    private var disassemblerLabels: [ UInt16: String ]
+    {
+        self.computer.bus.devices.reduce( into: [ UInt16: String ]() )
+        {
+            result, mapping in if let rom = mapping.device as? ROM
+            {
+                rom.labels.forEach { result[ $0.key ] = $0.value }
+            }
+        }
+    }
+
+    private var disassemblerComments: [ UInt16: String ]
+    {
+        self.computer.bus.devices.reduce( into: [ UInt16: String ]() )
+        {
+            result, mapping in if let rom = mapping.device as? ROM
+            {
+                rom.comments.forEach { result[ $0.key ] = $0.value }
+            }
+        }
+    }
+
+    public func printDisassembly( window: ManagedWindow, lines: Int )
+    {
+        guard lines > 0
+        else
+        {
+            return
+        }
+
+        let instructions = self.getInstructions( lines: lines )
+        let max          = instructions.reduce( into: [ Int: Int ]() )
+        {
+            result, line in line.enumerated().forEach
+            {
+                if let max = result[ $0.offset ], max >= $0.element.text.count
+                {
+                    return
+                }
+
+                result[ $0.offset ] = $0.element.text.count
+            }
+        }
+
+        instructions.map
+        {
+            $0.enumerated().map
+            {
+                if let max = max[ $0.offset ], $0.offset > 0
+                {
+                    if $0.offset == 1
+                    {
+                        return ( $0.element.color, " \( $0.element.text )".padding( toLength: max + 1, withPad: " ", startingAt: 0 ) )
+                    }
+                    else
+                    {
+                        return ( $0.element.color, "    \( $0.element.text )".padding( toLength: max + 4, withPad: " ", startingAt: 0 ) )
+                    }
+                }
+                else
+                {
+                    return ( $0.element.color, $0.element.text )
+                }
+            }
+        }
+        .forEach
+        {
+            self.printInstruction( window: window, instruction: $0 )
+        }
+    }
+
+    private func printInstruction( window: ManagedWindow, instruction: [ ( color: Color?, text: String ) ] )
+    {
+        instruction.forEach
+        {
+            if let color = $0.color
+            {
+                window.print( foreground: color, text: $0.text )
+            }
+            else
+            {
+                window.print( text: $0.text )
+            }
+        }
+
+        window.newLine()
+    }
+
+    private func getInstructions( lines: Int ) -> [ [ ( color: Color?, text: String ) ] ]
+    {
+        let stream       = MemoryDeviceStream( device: self.computer.bus, offset: self.computer.cpu.registers.PC )
+        let comments     = self.disassemblerComments
+        let disassembler = try? Disassembler( stream: stream, origin: self.computer.cpu.registers.PC, size: 0, instructions: 1, options: [], separator: "\0", comments: comments, labels: [ : ] )
+
+        guard let disassembler = disassembler
+        else
+        {
+            return [ [ ( color: .red, text: "???" ) ] ]
+        }
+
+        return ( 0 ..< lines ).map
+        {
+            _ in
+
+            let text = try? disassembler.disassemble()
+
+            guard let text = text
+            else
+            {
+                return [ ( color: .red, text: "???" ) ]
+            }
+
+            return text.components( separatedBy: "\0" ).enumerated().map
+            {
+                if $0.offset == 0
+                {
+                    return ( .cyan, $0.element )
+                }
+                else if $0.offset == 1
+                {
+                    return ( .yellow, $0.element )
+                }
+                else if $0.offset == 2
+                {
+                    return ( .green, $0.element )
+                }
+                else
+                {
+                    return ( .magenta, $0.element )
+                }
+            }
+        }
     }
 }
