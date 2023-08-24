c72a744a76162fc89d099e777eac1f8d5ca7cb6b
macmade <macmade@xs-labs.com>
2023-08-24 23:48:29 +0200

    chore(Tests): Unit tests for ORA

--- MOS6502-Tests/Instructions/Test-Instruction-ORA.swift
+++ MOS6502-Tests/Instructions/Test-Instruction-ORA.swift
@@ -28,26 +28,440 @@ import XCTest
 class Test_Instruction_ORA: Test_Instruction
 {
     func testAbsolute() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x32 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x1000 )
+        }
+    }
+
+    func testAbsolute_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x00 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1000 )
+        }
+    }
+
+    func testAbsolute_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0xAA ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x1000 )
+        }
+    }
+
     func testAbsoluteX() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x32, X: 0x10 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x1010 )
+        }
+    }
+
+    func testAbsoluteX_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x00, X: 0x10 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1010 )
+        }
+    }
+
+    func testAbsoluteX_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0xAA, X: 0x10 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x1010 )
+        }
+    }
+
     func testAbsoluteY() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteY,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x32, Y: 0x10 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x1010 )
+        }
+    }
+
+    func testAbsoluteY_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteY,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0x00, Y: 0x10 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1010 )
+        }
+    }
+
+    func testAbsoluteY_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .absoluteY,
+            operand16:       0x1000,
+            inputRegisters:  Registers( A: 0xAA, Y: 0x10 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x1010 )
+        }
+    }
+
     func testImmediate() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .immediate,
+            operand8:        0x4C,
+            inputRegisters:  Registers( A: 0x32 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+    }
+
+    func testImmediate_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .immediate,
+            operand8:        0x00,
+            inputRegisters:  Registers( A: 0x00 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+    }
+
+    func testImmediate_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .immediate,
+            operand8:        0x54,
+            inputRegisters:  Registers( A: 0xAA ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+    }
+
     func testIndirectX() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x32, X: 0x10 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x20 )
+            try bus.write( 0x4C, at: 0x1000 )
+        }
+    }
+
+    func testIndirectX_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x00, X: 0x10 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x20 )
+            try bus.write( 0x00, at: 0x1000 )
+        }
+    }
+
+    func testIndirectX_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0xAA, X: 0x10 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x20 )
+            try bus.write( 0x54, at: 0x1000 )
+        }
+    }
+
+    func testIndirectX_Wrap() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0x32, X: 0x20 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x0F )
+            try bus.write( 0x4C, at: 0x1000 )
+        }
+    }
+
+    func testIndirectX_Wrap_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0x00, X: 0x20 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x0F )
+            try bus.write( 0x00, at: 0x1000 )
+        }
+    }
+
+    func testIndirectX_Wrap_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0xAA, X: 0x20 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x0F )
+            try bus.write( 0x54, at: 0x1000 )
+        }
+    }
+
     func testIndirectY() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectY,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x32, Y: 0x10 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x10 )
+            try bus.write( 0x4C, at: 0x1010 )
+        }
+    }
+
+    func testIndirectY_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectY,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x00, Y: 0x10 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x10 )
+            try bus.write( 0x00, at: 0x1010 )
+        }
+    }
+
+    func testIndirectY_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .indirectY,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0xAA, Y: 0x10 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in
+
+            try bus.writeUInt16( 0x1000, at: 0x10 )
+            try bus.write( 0x54, at: 0x1010 )
+        }
+    }
+
     func testZeroPage() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x32 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x10 )
+        }
+    }
+
+    func testZeroPage_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x00 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x10 )
+        }
+    }
+
+    func testZeroPage_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0xAA ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x10 )
+        }
+    }
+
     func testZeroPageX() throws
-    {}
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x32, X: 0x10 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x20 )
+        }
+    }
+
+    func testZeroPageX_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0x00, X: 0x10 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x20 )
+        }
+    }
+
+    func testZeroPageX_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( A: 0xAA, X: 0x10 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x20 )
+        }
+    }
+
+    func testZeroPageX_Wrap() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0x32, X: 0x20 ),
+            outputRegisters: Registers( A: 0x7E, PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x4C, at: 0x0F )
+        }
+    }
+
+    func testZeroPageX_Wrap_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0x00, X: 0x20 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x0F )
+        }
+    }
+
+    func testZeroPageX_Wrap_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ORA",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( A: 0xAA, X: 0x20 ),
+            outputRegisters: Registers( A: 0xFE, PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x54, at: 0x0F )
+        }
+    }
 }
