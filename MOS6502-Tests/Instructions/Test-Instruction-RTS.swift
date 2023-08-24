594175465e35d4023337c1abc543887b1271b4a4
macmade <macmade@xs-labs.com>
2023-08-24 13:36:27 +0200

    chore(Tests): Unit tests for RTS

--- MOS6502-Tests/Instructions/Test-Instruction-RTS.swift
+++ MOS6502-Tests/Instructions/Test-Instruction-RTS.swift
@@ -28,5 +28,17 @@ import XCTest
 class Test_Instruction_RTS: Test_Instruction
 {
     func testImplied() throws
-    {}
+    {
+        try self.executeSingleInstruction(
+            instruction:     "RTS",
+            addressingMode:  .implied,
+            operands:        [],
+            origin:          0xFF00,
+            inputRegisters:  Registers( SP: 0xFD ),
+            outputRegisters: Registers( PC: 0x1001, SP: 0xFF )
+        )
+        {
+            cpu, bus, ram in try bus.writeUInt16( 0x1000, at: CPU.stackStart + 0xFE )
+        }
+    }
 }
