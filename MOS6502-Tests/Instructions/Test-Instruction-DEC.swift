e3f10d0b3a2c9a8fe5dcd7facd0fc48fe4eab82d
macmade <macmade@xs-labs.com>
2023-08-24 17:20:57 +0200

    chore(Tests): Unit tests for DEC

--- MOS6502-Tests/Instructions/Test-Instruction-DEC.swift
+++ MOS6502-Tests/Instructions/Test-Instruction-DEC.swift
@@ -28,14 +28,242 @@ import XCTest
 class Test_Instruction_DEC: Test_Instruction
 {
     func testAbsolute() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absolute,
+            operand:         0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x42, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x41 )
+    }
+
+    func testAbsolute_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absolute,
+            operand:         0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x01, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x00 )
+    }
+
+    func testAbsolute_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absolute,
+            operand:         0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0xFF )
+    }
+
     func testAbsoluteX() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absoluteX,
+            operand:         0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x42, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x41 )
+    }
+
+    func testAbsoluteX_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absoluteX,
+            operand:         0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x01, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x00 )
+    }
+
+    func testAbsoluteX_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .absoluteX,
+            operand:         0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0xFF )
+    }
+
     func testZeroPage() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPage,
+            operand:         0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x42, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x41 )
+    }
+
+    func testZeroPage_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPage,
+            operand:         0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x01, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x00 )
+    }
+
+    func testZeroPage_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPage,
+            operand:         0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0xFF )
+    }
+
     func testZeroPageX() throws
-    {}
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x42, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x41 )
+    }
+
+    func testZeroPageX_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x01, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x00 )
+    }
+
+    func testZeroPageX_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0xFF )
+    }
+
+    func testZeroPageX_Wrap() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x42, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x41 )
+    }
+
+    func testZeroPageX_Wrap_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x01, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x00 )
+    }
+
+    func testZeroPageX_Wrap_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "DEC",
+            addressingMode:  .zeroPageX,
+            operand:         0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0xFF )
+    }
 }
