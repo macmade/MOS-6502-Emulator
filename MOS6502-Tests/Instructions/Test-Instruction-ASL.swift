069bc5bd4b53de1d8bc1e909771f458c9a026e97
macmade <macmade@xs-labs.com>
2023-08-25 11:36:06 +0200

    chore(Tests): Unit tests for ASL

--- MOS6502-Tests/Instructions/Test-Instruction-ASL.swift
+++ MOS6502-Tests/Instructions/Test-Instruction-ASL.swift
@@ -28,17 +28,548 @@ import XCTest
 class Test_Instruction_ASL: Test_Instruction
 {
     func testAbsolute() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x33, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x66 )
+    }
+
+    func testAbsolute_Carry() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xAA, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x54 )
+    }
+
+    func testAbsolute_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x00 )
+    }
+
+    func testAbsolute_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x55, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0xAA )
+    }
+
+    func testAbsolute_Carry_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x80, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x00 )
+    }
+
+    func testAbsolute_Carry_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absolute,
+            operand16:       0x1000,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xC0, at: 0x1000 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1000 ), 0x80 )
+    }
+
     func testAbsoluteX() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x33, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x66 )
+    }
+
+    func testAbsoluteX_Carry() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xAA, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x54 )
+    }
+
+    func testAbsoluteX_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x00 )
+    }
+
+    func testAbsoluteX_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x55, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0xAA )
+    }
+
+    func testAbsoluteX_Carry_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x80, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x00 )
+    }
+
+    func testAbsoluteX_Carry_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .absoluteX,
+            operand16:       0x1000,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xC0, at: 0x1010 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x1010 ), 0x80 )
+    }
+
     func testAccumulator() throws
-    {}
-    
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0x33 ),
+            outputRegisters: Registers( A: 0x66, PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+    }
+
+    func testAccumulator_Carry() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0xAA ),
+            outputRegisters: Registers( A: 0x54, PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+    }
+
+    func testAccumulator_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0x00 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+    }
+
+    func testAccumulator_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0x55 ),
+            outputRegisters: Registers( A: 0xAA, PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+    }
+
+    func testAccumulator_Carry_Zero() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0x80 ),
+            outputRegisters: Registers( A: 0x00, PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+    }
+
+    func testAccumulator_Carry_Negative() throws
+    {
+        try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .accumulator,
+            operands:        [],
+            inputRegisters:  Registers( A: 0xC0 ),
+            outputRegisters: Registers( A: 0x80, PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+    }
+
     func testZeroPage() throws
-    {}
-    
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x33, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x66 )
+    }
+
+    func testZeroPage_Carry() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xAA, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x54 )
+    }
+
+    func testZeroPage_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x00 )
+    }
+
+    func testZeroPage_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x55, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0xAA )
+    }
+
+    func testZeroPage_Carry_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x80, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x00 )
+    }
+
+    func testZeroPage_Carry_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPage,
+            operand8:        0x10,
+            inputRegisters:  Registers(),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xC0, at: 0x10 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x10 ), 0x80 )
+    }
+
     func testZeroPageX() throws
-    {}
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x33, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x66 )
+    }
+
+    func testZeroPageX_Carry() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xAA, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x54 )
+    }
+
+    func testZeroPageX_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x00 )
+    }
+
+    func testZeroPageX_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x55, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0xAA )
+    }
+
+    func testZeroPageX_Carry_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x80, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x00 )
+    }
+
+    func testZeroPageX_Carry_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0x10,
+            inputRegisters:  Registers( X: 0x10 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xC0, at: 0x20 )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x20 ), 0x80 )
+    }
+
+    func testZeroPageX_Wrap() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x33, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x66 )
+    }
+
+    func testZeroPageX_Wrap_Carry() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xAA, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x54 )
+    }
+
+    func testZeroPageX_Wrap_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x00, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x00 )
+    }
+
+    func testZeroPageX_Wrap_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 0, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x55, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0xAA )
+    }
+
+    func testZeroPageX_Wrap_Carry_Zero() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 1, N: 0 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0x80, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x00 )
+    }
+
+    func testZeroPageX_Wrap_Carry_Negative() throws
+    {
+        let result = try self.executeSingleInstruction(
+            instruction:     "ASL",
+            addressingMode:  .zeroPageX,
+            operand8:        0xEF,
+            inputRegisters:  Registers( X: 0x20 ),
+            outputRegisters: Registers( PS: Flags( C: 1, Z: 0, N: 1 ) )
+        )
+        {
+            cpu, bus, ram in try bus.write( 0xC0, at: 0x0F )
+        }
+
+        XCTAssertEqual( try result.bus.read( at: 0x0F ), 0x80 )
+    }
 }
